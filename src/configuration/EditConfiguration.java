6972e4fac3146745eab9b52d27a22c43ef74e369
Lukasz <upaciorek@gmail.com>
2020-04-25 19:59:08 +0200

    working on configuration

--- src/configuration/EditConfiguration.java
+++ src/configuration/EditConfiguration.java
@@ -10,6 +10,7 @@ public class EditConfiguration extends JFrame {
         configurationFrame = new JFrame("Edit configuration");
         configurationFrame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         configurationFrame.setSize(300, 400);
+        addConfigurationFields();
 
         configurationFrame.setVisible(true);
         centerWindowOnScreen();
@@ -18,5 +19,35 @@ public class EditConfiguration extends JFrame {
     private void centerWindowOnScreen() {
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         configurationFrame.setLocation(dim.width / 2 - configurationFrame.getSize().width / 2, dim.height / 2 - configurationFrame.getSize().height / 2);
+    }
+
+    private void addConfigurationFields() {
+        JPanel configurationFieldsPane = new JPanel();
+
+        GridLayout layout = new GridLayout(3,2);
+
+        JPanel namePane = new JPanel();
+        JLabel nameLabel = new JLabel("Name:");
+        JTextField nameField = new JTextField(15);
+        namePane.add(nameLabel);
+        namePane.add(nameField);
+
+        JPanel hostPane = new JPanel();
+        JLabel hostLabel = new JLabel("Host: ");
+        JTextField hostField = new JTextField(15);
+        hostPane.add(hostLabel);
+        hostPane.add(hostField);
+
+        JPanel themePane = new JPanel();
+        JLabel themeLabel = new JLabel("Theme:");
+        JTextField themeField = new JTextField(14);
+        themePane.add(themeLabel);
+        themePane.add(themeField);
+
+        configurationFieldsPane.add(namePane);
+        configurationFieldsPane.add(hostPane);
+        configurationFieldsPane.add(themePane);
+
+        configurationFrame.add(configurationFieldsPane);
     }
 }
